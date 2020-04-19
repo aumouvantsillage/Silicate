6af586d0f3affa2b603aa315f2bcc7f9ae91317b
Guillaume Savaton <guillaume@baierouge.fr>
2020-04-19 17:33:33 +0200

    Added mode and type info in interfaces

--- examples/fifo.rkt
+++ examples/fifo.rkt
@@ -4,10 +4,12 @@
 (require "../src/component.rkt")
 (require "../src/std.rkt")
 
-(interface producer-consumer (data valid ready))
+(interface producer ([out data  T]
+                     [out valid boolean]
+                     [in  ready boolean]))
 
-(define (producer delay out)
-  (define out-ready (interface-ref out producer-consumer-ready))
+(define (source delay out)
+  (define out-ready (interface-ref out producer-ready))
 
   (define timer-max (sub1 delay))
   (define timer (register timer-max
@@ -21,13 +23,13 @@
   (define out-done (.and out-valid out-ready))
   (define out-data (register/e 0 out-done (.add1 out-data)))
 
-  (interface-set! out producer-consumer-valid out-valid)
-  (interface-set! out producer-consumer-data  out-data))
+  (interface-set! out producer-valid out-valid)
+  (interface-set! out producer-data  out-data))
 
 (define (fifo len in out)
-  (define in-valid  (interface-ref in  producer-consumer-valid))
-  (define in-data   (interface-ref in  producer-consumer-data))
-  (define out-ready (interface-ref out producer-consumer-ready))
+  (define in-valid  (interface-ref in  producer-valid))
+  (define in-data   (interface-ref in  producer-data))
+  (define out-ready (interface-ref out producer-ready))
 
   (define count (register/e 0 (.xor in-done out-done)
                   (.if in-done
@@ -54,13 +56,13 @@
   (define out-done (.and out-valid out-ready))
   (define out-data (.if is-empty in-data (.vector-ref data read-index)))
 
-  (interface-set! in  producer-consumer-ready in-ready)
-  (interface-set! out producer-consumer-valid out-valid)
-  (interface-set! out producer-consumer-data  out-data))
+  (interface-set! in  producer-ready in-ready)
+  (interface-set! out producer-valid out-valid)
+  (interface-set! out producer-data  out-data))
 
-(define (consumer delay in)
-  (define in-valid (interface-ref in producer-consumer-valid))
-  (define in-data  (interface-ref in producer-consumer-data))
+(define (sink delay in)
+  (define in-valid (interface-ref in producer-valid))
+  (define in-data  (interface-ref in producer-data))
 
   (define timer-max (static (sub1 delay)))
   (define timer (register/re 0 in-done (.< timer timer-max) (.add1 timer)))
@@ -68,23 +70,23 @@
   (define in-ready (.= timer timer-max))
   (define in-done  (.and in-valid in-ready))
 
-  (interface-set! in producer-consumer-ready in-ready))
+  (interface-set! in producer-ready in-ready))
 
-(define producer-to-fifo (make-producer-consumer))
-(define fifo-to-consumer (make-producer-consumer))
+(define fifo-in (make-producer))
+(define fifo-out (make-producer))
 
-(producer 6 producer-to-fifo)
-(fifo 4 producer-to-fifo fifo-to-consumer)
-(consumer 3 fifo-to-consumer)
+(source 6 fifo-in)
+(fifo   4 fifo-in fifo-out)
+(sink   3 fifo-out)
 
 (define (b2i lst)
   (for/list ([b (in-list lst)])
     (if b 1 0)))
 
 (define n 40)
-(printf "~a: ~a~n" "in-data  "      (signal-take (interface-ref producer-to-fifo producer-consumer-data)  n))
-(printf "~a: ~a~n" "in-valid " (b2i (signal-take (interface-ref producer-to-fifo producer-consumer-valid) n)))
-(printf "~a: ~a~n" "in-ready " (b2i (signal-take (interface-ref producer-to-fifo producer-consumer-ready) n)))
-(printf "~a: ~a~n" "out-data "      (signal-take (interface-ref fifo-to-consumer producer-consumer-data)  n))
-(printf "~a: ~a~n" "out-valid" (b2i (signal-take (interface-ref fifo-to-consumer producer-consumer-valid) n)))
-(printf "~a: ~a~n" "out-ready" (b2i (signal-take (interface-ref fifo-to-consumer producer-consumer-ready) n)))
+(printf "~a: ~a~n" "in-data  "      (signal-take (interface-ref fifo-in  producer-data)  n))
+(printf "~a: ~a~n" "in-valid " (b2i (signal-take (interface-ref fifo-in  producer-valid) n)))
+(printf "~a: ~a~n" "in-ready " (b2i (signal-take (interface-ref fifo-in  producer-ready) n)))
+(printf "~a: ~a~n" "out-data "      (signal-take (interface-ref fifo-out producer-data)  n))
+(printf "~a: ~a~n" "out-valid" (b2i (signal-take (interface-ref fifo-out producer-valid) n)))
+(printf "~a: ~a~n" "out-ready" (b2i (signal-take (interface-ref fifo-out producer-ready) n)))
--- src/component.rkt
+++ src/component.rkt
@@ -11,35 +11,38 @@
 (define-for-syntax (format-ctor-id intf-id)
   (format-id intf-id "make-~a" intf-id))
 
-; Create a plain interface object as a struct of boxes.
-; TODO hierarchical ports.
+; Syntax of interfaces:
+;
+; intf ::= (interface intf-id ([mode field-id type] ...))
+; mode ::= in | out | use | reverse
+; type ::= intf-id | data-type-spec
+;
+; TODO mode and data-type-spec are ignored
 (define-syntax (interface stx)
   (syntax-case stx ()
     [(_ intf-id (field ...))
-     ; Format the id of the constructor for this interface
      (with-syntax ([intf-ctor-id (format-ctor-id #'intf-id)])
        #`(begin
            ; Create a struct for this interface.
            (struct intf-id #,(for/list ([f (syntax->list #'(field ...))])
                                ; Keep only the name of each field.
                                (syntax-case f ()
-                                 [(field-id field-intf-id)  #'field-id]
-                                 [field-id                  #'field-id])))
+                                 [(_ field-id _)  #'field-id])))
            ; Create a constructor for this interface.
            (define (intf-ctor-id)
              ; Call the default constructor and initialize each field.
              (intf-id #,@(for/list ([f (syntax->list #'(field ...))])
                            ; Call a constructor for each field.
                            (syntax-case f ()
-                             ; If the field is specified as a pair (field-id field-intf-id),
-                             ; call the constructor for the target interface.
-                             [(field-id field-intf-id)
-                              (with-syntax ([field-intf-ctor-id (format-ctor-id #'field-intf-id)])
-                                #'(field-intf-ctor-id))]
-                             ; If the field is specified as a simple id,
+                             ; If the field is a plain input or output,
                              ; create an empty box.
-                             [field-id
-                              #'(box #f)]))))))]))
+                             [(in  field-id _) #'(box #f)]
+                             [(out field-id _) #'(box #f)]
+                             ; If the field uses an interface,
+                             ; call the constructor for the target interface.
+                             [(_ field-id field-intf-id)
+                              (with-syntax ([field-intf-ctor-id (format-ctor-id #'field-intf-id)])
+                                #'(field-intf-ctor-id))]))))))]))
 
 ; Get a proxy to a signal from an interface of the current component.
 ; Transforms: (interface-ref a b c d)

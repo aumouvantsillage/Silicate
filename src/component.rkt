fa3b1dc7df729c8089ba0e54af164cc9e3ade10e
Guillaume Savaton <guillaume@baierouge.fr>
2020-04-19 16:56:39 +0200

    Added support for hierarchical interfaces

--- src/component.rkt
+++ src/component.rkt
@@ -8,16 +8,38 @@
     interface-ref
     interface-set!)
 
+(define-for-syntax (format-ctor-id intf-id)
+  (format-id intf-id "make-~a" intf-id))
+
 ; Create a plain interface object as a struct of boxes.
 ; TODO hierarchical ports.
 (define-syntax (interface stx)
   (syntax-case stx ()
-    [(_ name (field ...))
-     (with-syntax ([make-name (format-id #'name "make-~a" #'name)])
-       #'(begin
-           (struct name (field ...))
-           (define (make-name)
-             (name (box (void 'field)) ...))))]))
+    [(_ intf-id (field ...))
+     ; Format the id of the constructor for this interface
+     (with-syntax ([intf-ctor-id (format-ctor-id #'intf-id)])
+       #`(begin
+           ; Create a struct for this interface.
+           (struct intf-id #,(for/list ([f (syntax->list #'(field ...))])
+                               ; Keep only the name of each field.
+                               (syntax-case f ()
+                                 [(field-id field-intf-id)  #'field-id]
+                                 [field-id                  #'field-id])))
+           ; Create a constructor for this interface.
+           (define (intf-ctor-id)
+             ; Call the default constructor and initialize each field.
+             (intf-id #,@(for/list ([f (syntax->list #'(field ...))])
+                           ; Call a constructor for each field.
+                           (syntax-case f ()
+                             ; If the field is specified as a pair (field-id field-intf-id),
+                             ; call the constructor for the target interface.
+                             [(field-id field-intf-id)
+                              (with-syntax ([field-intf-ctor-id (format-ctor-id #'field-intf-id)])
+                                #'(field-intf-ctor-id))]
+                             ; If the field is specified as a simple id,
+                             ; create an empty box.
+                             [field-id
+                              #'(box #f)]))))))]))
 
 ; Get a proxy to a signal from an interface of the current component.
 ; Transforms: (interface-ref a b c d)

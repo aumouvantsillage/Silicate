1f0a2771c71fb0f51beb020c7eecdb702108dc58
Guillaume Savaton <guillaume@baierouge.fr>
2020-04-26 15:53:27 +0200

    Interface generation: split macros

--- src/component.rkt
+++ src/component.rkt
@@ -11,10 +11,9 @@
 (define-for-syntax (format-ctor-id intf-id)
   (format-id intf-id "make-~a" intf-id))
 
-(define-for-syntax (interface-to-struct intf-id fields)
-  (writeln intf-id)
-  #`(struct #,intf-id
-      #,(for/list ([f fields])
+(define-syntax-rule (interface-to-struct intf-id field ...)
+  (struct intf-id
+      #,(for/list ([f (syntax->list #'(field ...))])
           (syntax-case f ()
             ; Keep only the name of each field.
             [(_ field-id _) #'field-id]))))

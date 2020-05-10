18d9f27e36f5f95770432a4ab27ddc097dd161fe
Guillaume Savaton <guillaume@baierouge.fr>
2020-05-10 10:44:43 +0200

    Added tests for components

---
+++ tests/component-tests.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require rackunit)
+(require "../src/component.rkt")
+
+(provide component-tests)
+
+(define component-tests
+  (test-suite "Components"
+    (test-case "Interface instance has boxes for simple ports"
+      (interface I () ([a in 'integer] [b out 'integer]))
+      (define an-I (make-I))
+      (check-pred box? (I-a an-I))
+      (check-pred box? (I-b an-I)))))
